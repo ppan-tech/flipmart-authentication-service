8dc75235a2c0da7500d2e2dbb36d4b8643e75c75
Puneet Pant <punepant@gmail.com>
2025-06-19 08:44:02 -0700

    initial commit:structure & dependencies

---
+++ src/main/java/com/unflipkart/services/unflipkartauthservice/UnflipkartApplication.java
@@ -0,0 +1,13 @@
+package com.unflipkart.services.unflipkartauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnflipkartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnflipkartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unflipkart/services/unflipkartauthservice/UnflipkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unflipkart.services.unflipkartauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnflipkartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
